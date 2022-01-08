01821b6e1dd068f5c0afd1a7a629bbd8ccafbda4
HenryDarts <97244438+HenryDarts@users.noreply.github.com>
2022-01-08 20:46:06 +0400

    Update index.php

--- index.php
+++ index.php
@@ -115,7 +115,7 @@
                 <div class="row h-100 align-items-center py-5">
 
                     <div class="col-lg-6">
-                        <h1 class="display-4">პროექტის შესახებ შესახებ</h1>
+                        <h1 class="display-4">პროექტის შესახებ</h1>
                         <p class="lead text-muted mb-0">2021 წლის სექტემბრიდან, ფორუმი ორგანიზაციასთან „Jaan Tõnnissoni Instituut“ (ესტონეთი) თანამშრომლობით ახორციელებს პროექტს „ციფრული სამოქალაქო განათლების ოლიმპიადა მდგრადი განვითარებისთვის“</p>
                     </div>
 
